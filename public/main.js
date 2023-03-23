d86dc05b76f74857977fedebe32ec57802421e2f
Derek Wheeler <frozenarrow73@gmail.com>
2023-03-23 15:37:22 -0600

    fixed bug with /tryCatch

--- public/main.js
+++ public/main.js
@@ -1,6 +1,37 @@
-let buttonForm = document.querySelector("form")
+let buttonForm = document.getElementById("form1")
+let addForm = document.getElementById("form2")
+let deleteBtn = document.getElementById("clear")
+let input = document.querySelector("input")
 
-buttonForm.addEventListener("click", (event) => {
+// include and initialize the rollbar library with your access token
+var Rollbar = require('rollbar')
+var rollbar = new Rollbar({
+  accessToken: '9eec08d82f024dcaaf64154f7ba30479',
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+})
+
+// record a generic message and send it to Rollbar
+rollbar.log('Hello world!')
+
+
+
+buttonForm.addEventListener("submit", (event) => {
     event.preventDefault()
-    axios.get("http://localhost:4000/tryCatch")
+    axios.get("http://3.101.89.235/tryCatch").then((result) => {
+
+    }).catch((err) => {
+        rollbar.warning(err, "unable to connect to /tryCatch")
+    })
+})
+
+addForm.addEventListener("submit", (event) => {
+    event.preventDefault()
+    let text = input.value
+    mabod ={
+        input: text
+    }
+    axios.post("http://3.101.89.235/add", mabod).then((result) => {
+        
+    })
 })
